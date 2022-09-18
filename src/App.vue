bc455d171c5cdb6a4099a053bcf405b3aa02b37f
tictac <fominsartems947@gmail.com>
2022-09-18 20:35:13 +0300

    setup tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { reactive } from "vue"
 import HelloWorld from "./components/HelloWorld.vue"
 
@@ -13,32 +11,20 @@ const something = "sadasdsadsa"
 </script>
 
 <template>
-	<div>
-		<h2>INcrement {{ count.count }}</h2>
-		<p @click="increment">increment</p>
-		<p @click="decrement">increment</p>
+	<div class="bg-slate-900">
+		<div class="bg-cyan-600">
+			<h2>INcrement {{ count.count }}</h2>
+			<p @click="increment" class="bg-red-500">increment</p>
+			<p @click="decrement">increment</p>
+		</div>
+		<div>
+			<a href="https://vitejs.dev" target="_blank">
+				<img src="/vite.svg" class="logo" alt="Vite logo" />
+			</a>
+			<a href="https://vuejs.org/" target="_blank">
+				<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+			</a>
+		</div>
+		<HelloWorld msg="Vite + Vue" />
 	</div>
-	<div>
-		<a href="https://vitejs.dev" target="_blank">
-			<img src="/vite.svg" class="logo" alt="Vite logo" />
-		</a>
-		<a href="https://vuejs.org/" target="_blank">
-			<img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-		</a>
-	</div>
-	<HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-	height: 6em;
-	padding: 1.5em;
-	will-change: filter;
-}
-.logo:hover {
-	filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-	filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
