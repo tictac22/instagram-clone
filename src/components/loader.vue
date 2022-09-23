af0a5c0d7430963805d638144062edfbfeedc1d1
tictac <fominsartems947@gmail.com>
2022-09-24 00:43:04 +0300

    add loader

---
+++ src/components/loader.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="h-screen relative">
+		<img
+			src="instagram.png"
+			alt="instagram"
+			class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+		/>
+		<img
+			src="meta.png"
+			alt="meta"
+			class="absolute bottom-8 left-1/2 -translate-x-1/2 w-[72px] h-[37px]"
+		/>
+	</div>
+</template>
