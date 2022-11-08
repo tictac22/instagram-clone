77cd8203181c7c3b009aa6b5802b8e2d46a0f604
tictac <fominsartems947@gmail.com>
2022-11-08 14:23:33 +0200

    setup unit testing with vitest

---
+++ src/__tests__/test.spec.ts
@@ -0,0 +1,31 @@
+import { beforeEach, describe, expect, it } from "vitest"
+
+import { mount, VueWrapper } from "@vue/test-utils"
+import Test from "../components/test.vue"
+
+const enum dataAttrs {
+	counter = '[data-test="counter"]',
+	increment = '[data-test="increment"]',
+	decrement = '[data-test="decrement"]'
+}
+describe("test component", () => {
+	let component: VueWrapper
+	beforeEach(() => {
+		component = mount(Test)
+	})
+	it("It mounts succesfullyy ", () => {
+		const counter = component.find(dataAttrs.counter)
+		expect(counter.text()).toContain("counter 0")
+		expect(component.element).toMatchSnapshot()
+	})
+	it("it increments correctly", async () => {
+		await component.find(dataAttrs.increment).trigger("click")
+		const counter = component.find(dataAttrs.counter)
+		expect(counter.text()).toContain("counter 1")
+	})
+	it("it decrements correctly", async () => {
+		await component.find(dataAttrs.decrement).trigger("click")
+		const counter = component.find(dataAttrs.counter)
+		expect(counter.text()).toContain("-1")
+	})
+})
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+
+const counter = ref(0)
+const increment = () => counter.value++
+const decrement = () => counter.value--
+</script>
 
 <template>
 	<div>
-		<h1>333</h1>
-		<div>something</div>
-		<section>ewqeqwewqsdsadsa</section>
-		<main>dsads</main>
+		<h1 data-test="counter">counter {{ counter }}</h1>
+		<button data-test="increment" @click="increment">increment</button>
+		<button data-test="decrement" @click="decrement">decrement</button>
 	</div>
 </template>
